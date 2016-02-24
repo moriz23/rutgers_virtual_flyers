540561546529c052649b61106b782cd8a9b912cd
Mohammed Rizvi <morizvi07@gmail.com>
2016-02-24 15:45:37 -0500

    Installed npm packages

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-// Inports all packages
+//NPM PACKAGES INSTALLED
 var express           = require('express');
 var expressHandlebars = require('express-handlebars');
 var session           = require('express-session');
 var Sequelize         = require('sequelize');
-var app               = express();
-var bcrypt            = require('bcryptjs');
-var bodyParser        = require('body-parser');
-var PORT = process.env.PORT || 8080;
-
-// Connects to database
-var sequelize = new Sequelize('rutgers_db', 'root');
-
-// Passport packages
 var passport          = require('passport');
 var passportLocal     = require('passport-local');
+var bcrypt            = require('bcryptjs');
+var bodyParser        = require('body-parser');
+var app               = express();
+var PORT = process.env.PORT || 8070;
