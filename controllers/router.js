c909c448fd4d3348e350da8ef67df4c8e5dfeff1
Mohammed Rizvi <morizvi07@gmail.com>
2016-03-04 13:54:06 -0500

    Put a color theme, background image, working on registration pop in

--- controllers/router.js
+++ controllers/router.js
@@ -64,7 +64,6 @@ router.post('/:business/reviews', function(req, res) {
       var comment = req.body.comment;
       console.log(comment);
       req.session.authenticated = review;
-      res.redirect(BUSINESS_NAME)
       res.render('yelp-business', {
         reviews: comment,
         layout: 'yelp-business'
